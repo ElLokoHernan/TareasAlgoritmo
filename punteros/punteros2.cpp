02af2258fa6b713d290d636798a83b957ae5a464
ElLokoHernan <85263208+ElLokoHernan@users.noreply.github.com>
2025-03-26 20:44:52 -0300

    seguimos usando punteros

--- punteros/punteros2.cpp
+++ punteros/punteros2.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
 //los parametros son variables punteros.
 void sumar(int *a, int *b, int *c);
+void PedirDatos();
 
 int x,y,z;
 int main(){
-    
-    scanf("%d", &x);
-    scanf("%d", &y);
-    scanf("%d", &z);//no importa el valor que le asignamos a esta variable, de igual forma, toma el valor que agregamos en la funcion sumar
+    printf("\t**PUNTEROS**\n");
+    PedirDatos();
 //notece que los parametros que pasamos son las direcciones de a,b y c;
     sumar(&x, &y, &z);
 //mostramos el valor almacenado en c.
@@ -17,4 +16,12 @@ int main(){
 //pasamos los parametros en forma de punteros.
 void sumar(int *a, int *b, int *c){
     *c = *a + *b;
+}
+
+void PedirDatos(){
+    printf("ingresar un valor entero para sumar :");
+    scanf("%d", &x);
+    printf("ingresar otro valor entero para sumar");
+    scanf("%d", &y);
+    //scanf("%d", &z);//no importa el valor que le asignamos a esta variable, de igual forma, toma el valor que agregamos en la funcion sumar
 }
