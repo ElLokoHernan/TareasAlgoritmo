cdbc299441fe7133230cea5d3506ee550ddf62e1
ElLokoHernan <85263208+ElLokoHernan@users.noreply.github.com>
2025-04-01 01:15:43 -0300

    agregamos la funcion de mostrar los datos de la lista

--- ListasEnlazadas/FuncionCompletarCamposDelNodo.cpp
+++ ListasEnlazadas/FuncionCompletarCamposDelNodo.cpp
@@ -20,6 +20,7 @@ typedef struct nodoPacientes{
 void inicializarLista();
 void insertarPaciente(tPaciente);
 tPaciente ingresarPaciente();
+void mostrarPacientes(tListaPacientes *);
 
 tListaPacientes * pacientes;
 
@@ -27,6 +28,7 @@ int main(){
     inicializarLista();
     tPaciente nuevoPaciente = ingresarPaciente();
     insertarPaciente(nuevoPaciente);
+    mostrarPacientes(pacientes);
     return 0;
 }
 
@@ -56,4 +58,20 @@ tPaciente ingresarPaciente(){
     scanf("%f", &paciente.peso);
   
     return paciente;
+}
+//En esta segunda instacion agreamos la funcion que nos permite ver los datos agregados a nuestro programa, la verdad estoy sorprendido con los resultados porque todos esta funcionando 
+//a tener en cuenta que para mostrar la lista de paciente es necesario pasarle como parametro una variable puntero. al mostrar la lista, en el main, debemos pasar como parametro la variable apuntada.
+void mostrarPacientes(tListaPacientes * pListaPacientes){
+    tListaPacientes * listaAux = pListaPacientes;
+    printf("\n\tLISTA DE PACIENTES");
+    while(listaAux!=NULL){
+        printf("\n 0Edad del paciente : %d", listaAux ->paciente.edad);
+        printf("\n Nombre del paciente : %s", listaAux->paciente.nombre);
+        printf("\n Peso del paciente : %2.2f", listaAux->paciente.peso);
+        printf("\n--------");
+        listaAux = listaAux->siguiente;
+        printf("Saltamos la linea ");
+    }
+    printf("|n ***");
+
 }
